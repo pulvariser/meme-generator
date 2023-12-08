eb8bc44ee87663b350b43514c95b08600a04ad47
mayur burange <mayurburange72@gmail.com>
2023-12-08 16:02:45 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Images from "./Assets/troll-face.png"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src={Images}
+                alt ="troll face img"
+                
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
